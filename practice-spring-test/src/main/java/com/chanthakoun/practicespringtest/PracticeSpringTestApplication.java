5931c1e58de36a6e303d76f232d379072ba3a2d6
Andrew Chanthakoun <chanthakoun2002@gmail.com>
2025-09-12 18:54:42 -0700

    initial commit

---
+++ practice-spring-test/src/main/java/com/chanthakoun/practicespringtest/PracticeSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.chanthakoun.practicespringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpringTestApplication.class, args);
+    }
+
+}
---
+++ practice-spring-test/src/test/java/com/chanthakoun/practicespringtest/PracticeSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanthakoun.practicespringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
